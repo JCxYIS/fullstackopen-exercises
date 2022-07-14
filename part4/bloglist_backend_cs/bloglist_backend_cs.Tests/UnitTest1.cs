0ff3bc5d94afa048143aab464d05ef94d858d9d5
JCxYIS <jsn811@gmail.com>
2022-07-14 23:32:59 +0800

    ~4.7

--- part4/bloglist_backend_cs/bloglist_backend_cs.Tests/UnitTest1.cs
+++ part4/bloglist_backend_cs/bloglist_backend_cs.Tests/UnitTest1.cs
@@ -59,5 +59,73 @@ namespace bloglist_backend_cs.Tests
             };
             Assert.IsTrue(ListHelper.FavoriteBlog(blogs) == blogs[1]);
         }
+
+        [Test, Description("the author who has the largest amount of blogs. The return value also contains the number of blogs the top author has")]
+        public void MostBlogTest()
+        {
+            var blogs = new List<Blog>();            
+            int maxBlog = 0;
+            string maxAuthor = "";
+            
+            // define some dummy authors
+            for(int i = 0; i < 10; i++)
+            {
+                string author = "author" + i;
+                int blogCount = new Random().Next(30);
+                for(int j = 0; j < blogCount; j++)
+                {
+                    blogs.Add(new Blog
+                    {
+                        author = author,
+                    });
+                }
+                if (blogCount > maxBlog)
+                {
+                    maxBlog = blogCount;
+                    maxAuthor = author;
+                }
+            }
+
+            // execute
+            dynamic result = ListHelper.MostBlogAuthor(blogs.ToArray());
+            Assert.IsTrue(result.blogs == maxBlog);
+            Assert.IsTrue(result.author == maxAuthor);
+        }
+
+        [Test, Description(" The function returns the author, whose blog posts have the largest amount of likes. The return value also contains the total number of likes that the author has received")]
+        public void MostLikeTest()
+        {
+            var blogs = new List<Blog>();
+            int maxLike = 0;
+            string maxAuthor = "";
+
+            // define some dummy authors
+            for (int i = 0; i < 10; i++)
+            {
+                string author = "author" + i;
+                int blogCount = new Random().Next(30);
+                int likeCount = 0;
+                for (int j = 0; j < blogCount; j++)
+                {
+                    int thisLikeCount = new Random().Next(10000);
+                    blogs.Add(new Blog
+                    {
+                        author = author,
+                        likes = thisLikeCount,
+                    });
+                    likeCount += thisLikeCount;
+                }
+                if (likeCount > maxLike)
+                {
+                    maxLike = likeCount;
+                    maxAuthor = author;
+                }
+            }
+
+            // execute
+            var result = ListHelper.MostLikeAuthor(blogs.ToArray());
+            Assert.IsTrue(result.likes == maxLike);
+            Assert.IsTrue(result.author == maxAuthor);
+        }        
     }
 }
--- part4/bloglist_backend_cs/bloglist_backend_cs/Utils/ListHelper.cs
+++ part4/bloglist_backend_cs/bloglist_backend_cs/Utils/ListHelper.cs
@@ -19,7 +19,7 @@ namespace bloglist_backend_cs.Utils
             return total;
         }
 
-        public static Blog FavoriteBlog(Blog[] blogs)
+        public static Blog? FavoriteBlog(Blog[] blogs)
         {
             int max = 0;
             Blog maxBlog = null;
@@ -32,6 +32,80 @@ namespace bloglist_backend_cs.Utils
                 }
             }
             return maxBlog;
+        }
+
+        public class MostBlogAuthorResult
+        {
+            public string author;
+            public int blogs;
+        }
+        
+        public static MostBlogAuthorResult MostBlogAuthor(Blog[] blogs)
+        {
+            Dictionary<string, List<Blog>> authorBlog = new Dictionary<string, List<Blog>>();
+            foreach (var blog in blogs)
+            {
+                if (authorBlog.ContainsKey(blog.author))
+                {
+                    authorBlog[blog.author].Add(blog);
+                }
+                else
+                {
+                    authorBlog.Add(blog.author, new List<Blog>() { blog });
+                }
+            }
+            int max = 0;
+            string maxAuthor = "";
+            foreach (var author in authorBlog)
+            {
+                if (author.Value.Count > max)
+                {
+                    max = author.Value.Count;
+                    maxAuthor = author.Key;
+                }
+            }
+            return new MostBlogAuthorResult
+            {
+                blogs = max,
+                author = maxAuthor
+            };
+        }
+
+        public class MostLikeAuthorResult
+        {
+            public string author;
+            public int likes;
+        }
+
+        public static MostLikeAuthorResult MostLikeAuthor(Blog[] blogs)
+        {
+            Dictionary<string, int> authorBlog = new Dictionary<string, int>();
+            foreach (var blog in blogs)
+            {
+                if (authorBlog.ContainsKey(blog.author))
+                {
+                    authorBlog[blog.author] += blog.likes;
+                }
+                else
+                {
+                    authorBlog.Add(blog.author, blog.likes);
+                }
+            }
+            int max = 0;
+            string maxAuthor = "";
+            foreach (var author in authorBlog)
+            {
+                if (author.Value > max)
+                {
+                    max = author.Value;
+                    maxAuthor = author.Key;
+                }
+            }
+            return new MostLikeAuthorResult
+            {
+                likes = max,
+                author = maxAuthor
+            };
         }
     }
 }
